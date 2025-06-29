80f55a1e58bcf9df5fd52e0cd296009f58b90e0f
JongMoon <img1518@gmail.com>
2025-06-29 16:07:43 +0000

    관리자 시스템 작업 커밋

--- adminsystem/src/main/java/labcqrssummarize/domain/RequestPublish.java
+++ adminsystem/src/main/java/labcqrssummarize/domain/RequestPublish.java
@@ -8,6 +8,6 @@ import lombok.*;
 @Data
 @ToString
 public class RequestPublish extends AbstractEvent {
-//이거 코드 Command->Policy 흐름 때문에 잘못 생성된 코드 지우고, RequestContentDenied.java 생성하고 PolicyHandler.java에서 해당 부분 삭제해야함.
+//이거 코드 이전 도메인 Pub/Sub 구조때문에 생긴것.  RequestContentDenied.java 
     private Long id;
 }
